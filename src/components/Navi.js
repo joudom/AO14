bfbca9654c82a18b7cfb3200c7fb848501b27d23
Jesse Oudom <jesse.oudom@yahoo.com>
2022-12-31 17:41:58 -0500

    another attempt at fixing routes

--- src/components/Navi.js
+++ src/components/Navi.js
@@ -9,16 +9,16 @@ function Navi() {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="light">
       <Container className="navi ms-auto">
-        <Navbar.Brand as={Link} to='/AO14/'><img src="https://img.icons8.com/ios/50/null/football.png" alt='banner'/>AO14</Navbar.Brand>
+        <Navbar.Brand as={Link} to='/'><img src="https://img.icons8.com/ios/50/null/football.png" alt='banner'/>AO14</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav"  />
         <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="me-auto"></Nav>
           <Nav className="me-auto">
-            <Nav.Link as={Link} eventKey='0' to='/AO14/about'>About</Nav.Link>
-            <Nav.Link as={Link} eventKey='1' to='/AO14/contact'>Contact</Nav.Link>
+            <Nav.Link as={Link} eventKey='0' to='/about'>About</Nav.Link>
+            <Nav.Link as={Link} eventKey='1' to='/contact'>Contact</Nav.Link>
             <NavDropdown title="Media" id="collasible-nav-dropdown">
-              <NavDropdown.Item as={Link} eventKey='2' to='/AO14/media/videos'>Videos</NavDropdown.Item>
-              <NavDropdown.Item as={Link} eventKey='3' to='/AO14/media/photos'>Photos</NavDropdown.Item>
+              <NavDropdown.Item as={Link} eventKey='2' to='/media/videos'>Videos</NavDropdown.Item>
+              <NavDropdown.Item as={Link} eventKey='3' to='/media/photos'>Photos</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
