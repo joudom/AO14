ab8b6a54149904ca0cfc2c75f6a23eab6dd824d4
Jesse Oudom <jesse.oudom@yahoo.com>
2023-01-02 14:21:35 -0500

    updates video page

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -1,11 +1,32 @@
-
+import { useState, useEffect } from 'react';
+import { L30 } from 'react-isloading'
+import '../styles/Video.css';
 
 function Video() {
-    return (
-      <div className="video" style={{ display: "flex", alignItems: "center", justifyContent: "center", height: "92vh" }}>
-          <h1>COMING SOON</h1>
-      </div>
-    );
-  }
+  const [loading, setLoading] = useState(false)
+  useEffect(()=>{
+    setLoading(true)
+    setTimeout(()=>{
+      setLoading(false)
+    },5000)
+  },[])
+
+  return (
+    <div className="video-container">
+      {
+        loading?
+        <L30 style={{ height: "30rem", width: "30rem", position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)" }}/>
+        :
+        <div className='video-item'>
+        <h1>Fall 2022 Highlights</h1>
+        <iframe width="400" height="315" src="https://www.youtube-nocookie.com/embed/eGkCzp-LSYQ" 
+          title="Ashton Oudom - Fall 2022 Highlights" border="0" 
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen>
+        </iframe>
+        </div>
+      }
+    </div>
+  );
+}
   
   export default Video;
