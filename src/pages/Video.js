2d59d26f174d0f83eb2b9be688a5f65225e74e0c
Jesse Oudom <jesse.oudom@yahoo.com>
2023-01-02 14:41:49 -0500

    span and font added

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -18,7 +18,7 @@ function Video() {
         <L30 style={{ height: "30rem", width: "30rem", position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)" }}/>
         :
         <div className='video-item'>
-        <h1>Fall 2022 Highlights</h1>
+        <h1>Fall 2022 <span>Highlights</span></h1>
         <iframe width="400" height="315" src="https://www.youtube-nocookie.com/embed/eGkCzp-LSYQ" 
           title="Ashton Oudom - Fall 2022 Highlights" border="0" 
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen>
