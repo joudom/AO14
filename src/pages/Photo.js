cbae4d1a5e1dbbacc2163ec0e132bcb1f2d8cfe9
Jesse Oudom <jesse.oudom@yahoo.com>
2023-01-02 14:41:33 -0500

    styles updated

--- src/pages/Photo.js
+++ src/pages/Photo.js
@@ -18,14 +18,14 @@ function Photo() {
       loading?
       <L30 style={{ height: "30rem", width: "30rem", position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)" }}/>
       :
-      <>
+      <div className='photo-item'>
         <center>
           <h4>Photography by Nyree Mereus</h4>
           <h5>Sunday, December 04, 2022</h5>
           <h6>AFU South v. Elmbrook United</h6>
         </center>
         <Gallery />
-      </>
+      </div>
       }
     </div>
   );
