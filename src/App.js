c586980c82a125666fa710c930d114639c8530c8
Jesse Oudom <jesse.oudom@yahoo.com>
2023-01-01 22:22:17 -0500

    adds hashrouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navi from './components/Navi';
 import Home from './pages/Home';
 import About from './pages/About';
